a4ef79e05a8289a74b7c7377ea4c020b5b82136f
Hao Phan <nguyenhao.phan@gmail.com>
2022-06-13 01:40:43 +0300

    update user schema

--- api/src/users/user.schema.ts
+++ api/src/users/user.schema.ts
@@ -4,10 +4,18 @@ export type UserDocument = Document & {
   username: string
   password: string
   email?: string
+  resources?: {
+    name: string
+    quantity: number
+  }
 }
 
 export const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   email: { type: String, unique: true },
+  resources: {
+    name: String,
+    quantity: Number,
+  },
 })
